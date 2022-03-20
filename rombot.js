f1674e0c59c9c3f34d86978ab2865a017a7f9654
vanybhops <79766479+vanybhops@users.noreply.github.com>
2022-03-20 23:48:14 +0100

    Create rombot.js

---
+++ rombot.js
@@ -0,0 +1,111 @@
+const sleep = (milliseconds) => {return new Promise(resolve => setTimeout(resolve, milliseconds))};
+const fetch = require('node-fetch');
+const WebSocket  = require('ws');
+let token=""
+async function provjeracijene(chid,vrsta,msid) {
+    await fetch("https://prizmacomerc.ba/cijenovnik-otkupa-sekundarnih-sirovina/", {
+        "method": "GET",
+      }).then((res)=>{return res.text()}).then(x=>{cekiranje(x.match(/(?<=<td>)[0-9,;]+/g),vrsta,msid)})
+    async function cekiranje(arrayz,vrsta,msid) {
+        let ar=arrayz
+        if (!vrsta.includes("!rombot"))
+          return
+        vrsta=vrsta.replace("!rombot ","")
+        switch (vrsta) {
+            case "bakar":
+                sendmsg(chid,"cijena bakra je :"+ar[4]+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[4])/1.952).toFixed(2)+" EUR/kg"+
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[4])*3.883).toFixed(2)+" HRK/kg"+
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[4])*59.76).toFixed(2)+" RSD/kg",msid)
+                break
+            case "zeljezo":
+                sendmsg(chid,"cijena zeljeza je :"+ar[1]+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[1])/1.952).toFixed(2)+" EUR/kg"+
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[4])*3.883).toFixed(2)+" HRK/kg"+
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[4])*59.76).toFixed(2)+" RSD/kg",msid)
+                break
+            case "aluminij":
+                sendmsg(chid,"cijena aluminija je :"+ar[8]+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[8])/1.952).toFixed(2)+" EUR/kg"+
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[4])*3.883).toFixed(2)+" HRK/kg"+
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[4])*59.76).toFixed(2)+" RSD/kg",msid)
+                break
+            case "olovo":
+                sendmsg(chid,"cijena olova je :"+ar[11]+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[11])/1.952).toFixed(2)+" EUR/kg"+
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[4])*3.883).toFixed(2)+" HRK/kg"+
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[4])*59.76).toFixed(2)+" RSD/kg",msid)
+                break
+            case "akumulatori":
+                sendmsg(chid,"cijena akumulatora je :"+ar[12]+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[12])/1.952).toFixed(2)+" EUR/kg"+
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[4])*3.883).toFixed(2)+" HRK/kg"+
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[4])*59.76).toFixed(2)+" RSD/kg",msid)
+                break
+            default:
+                break;
+        }
+    }
+}
+async function getmsg(guid,msid) {
+  await sleep(300)
+  try {
+    await fetch(`https://discord.com/api/v9/channels/${guid}/messages?limit=1&around=${msid}`, {
+      "headers": {
+        "authorization": "OTQ1MDc0Mzk1NzA0MDc0Mjcx.Yiu_iA.I5aiDCoxZ7b8tqEgLrY_nz7FQXI",},
+      "method": "GET",
+    }).then(r=>{return r.json()}).then(
+      x=>{
+        if (x[0]["content"].toLowerCase()=="!rombot bakar"||"!rombot zeljezo"||"!rombot aluminij"||"!rombot olovo"||"!rombot akumulatori")
+          provjeracijene(guid,x[0]["content"].toLowerCase(),msid)
+        if(x[0]["content"].toLowerCase()=="!rombot pomozi mi ciganinu")
+          sendmsg(guid,"ROMBOT STIZE U POMOC\nlista metala u magacinu je: \nbakar\nzeljezo\naluminij\nolovo\nakumulatori",msid)
+        });
+  } catch (error){
+  }
+
+}
+function sendmsg(chid,ctx,msid) {
+    fetch(`https://discord.com/api/v9/channels/${chid}/messages`, {
+    "headers": {
+      "authorization": `${token}`,
+      "content-type": "application/json"},
+    "body": JSON.stringify({content:ctx,message_reference:{channel_id:`${chid}`,message_id:`${msid}`}}),
+    "method": "POST",
+  })
+}
+let socket = new WebSocket("wss://gateway.discord.gg/?v=6&encording=json");
+socket.onmessage = async function(event) {
+    let ejson = JSON.parse(event.data);
+    let e = JSON.stringify(event.data);
+
+    let payload={
+        "op": 2,
+        "d": {
+            "token": token,
+            "intents": 32767,
+            "properties": {
+                "$os": "windows",
+                "$browser": "chrome",
+                "$device": 'pc'}
+            }
+        }
+    async function hb(socket, interval){
+        while(true){
+            let hbpayload={
+                'op': 1,
+                'd': 'null'
+            };
+            socket.send(JSON.stringify(hbpayload));
+            await sleep(interval);
+        };
+    };
+    if(e.includes('heartbeat_interval')){
+        var interval = JSON.parse(event.data)['d']['heartbeat_interval'];
+        hb(socket, interval);
+        socket.send(JSON.stringify(payload));
+    };
+    if (ejson["t"]=="READY")
+      socket.send(JSON.stringify({"op":3,"d":{"status":"online","activities":[{
+          "name": "!rombot pomozi mi ciganinu",
+          "type": 0,}],
+        "since":1,"afk":false}}))
+    if (ejson["t"]=="MESSAGE_CREATE") {
+      getmsg(ejson['d']['channel_id'],ejson['d']['id'])
+    }
+}
