58181ae4419b1d8f5587e2da059c28ee3cb7f800
vanybhops <79766479+vanybhops@users.noreply.github.com>
2022-04-11 00:03:40 +0200

    efikasniji kod :3

--- rombot.js
+++ rombot.js
@@ -1,90 +1,59 @@
 const sleep = (milliseconds) => {return new Promise(resolve => setTimeout(resolve, milliseconds))};
 const fetch = require('node-fetch');
 const WebSocket  = require('ws');
-let podsjetnik=false
-let mybban=false
-let token="OTU4NDkzMDU5MTQ5MDk5MDI4.YkoUxQ.DWCOy-C1CJTlk3x3UlQsLT8ZVTs"
-async function randomdova(){
-	let rd=""
-	await fetch("http://ikb-berlin.de/islamske-teme/muhammed-as/dove-allahovog-poslanika-muhammeda-as/109").then(x=>{return x.text()}).then(x=>rd=x.match(/(?<=“)(.*)(?=”)/gm)[Math.floor(Math.random() * x.match(/(?<=“)(.*)(?=”)/gm).length)])
-	return rd}
+let token=""
+let mrtvi=["<@945074395704074271>"]
 async function provjeracijene(chid,vrsta,msid) {
     await fetch("https://prizmacomerc.ba/cijenovnik-otkupa-sekundarnih-sirovina/", {
         "method": "GET",
       }).then((res)=>{return res.text()}).then(x=>{cekiranje(x.match(/(?<=<td>)[0-9,;]+/g),vrsta,msid)})
-    async function cekiranje(arrayz,vrsta,msid) {
-        let ar=arrayz
-        if (!vrsta.includes("!rombot"))
-          return
-        vrsta=vrsta.replace("!rombot ","")
+    async function cekiranje(ar,vrsta,msid) {
         switch (vrsta) {
+            case "pomozi mi ciganinu":
+                sendmsg(chid,"```ROMBOT STIZE U POMOC\nlista metala u magacinu je: \nbakar\nzeljezo\naluminij\nolovo\nakumulatori```",msid)
+                break
             case "bakar":
-                sendmsg(chid,"cijena bakra je :"+ar[4].replace(",",".")+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[4].replace(",","."))/1.952).toFixed(2)+" EUR/kg"+
+                sendmsg(chid,"```cijena bakra je :"+ar[4].replace(",",".")+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[4].replace(",","."))/1.952).toFixed(2)+" EUR/kg"+
                 "\n\t\t\t\t\t\t\t"+(parseFloat(ar[4].replace(",","."))*3.883).toFixed(2)+" HRK/kg"+
-                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[4].replace(",","."))*59.76).toFixed(2)+" RSD/kg",msid)
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[4].replace(",","."))*59.76).toFixed(2)+" RSD/kg```",msid)
                 break
             case "zeljezo":
-                sendmsg(chid,"cijena zeljeza je :"+ar[1].replace(",",".")+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[1].replace(",","."))/1.952).toFixed(2)+" EUR/kg"+
+                sendmsg(chid,"```cijena zeljeza je :"+ar[1].replace(",",".")+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[1].replace(",","."))/1.952).toFixed(2)+" EUR/kg"+
                 "\n\t\t\t\t\t\t\t"+(parseFloat(ar[1].replace(",","."))*3.883).toFixed(2)+" HRK/kg"+
-                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[1].replace(",","."))*59.76).toFixed(2)+" RSD/kg",msid)
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[1].replace(",","."))*59.76).toFixed(2)+" RSD/kg```",msid)
                 break
             case "aluminij":
-                sendmsg(chid,"cijena aluminija je :"+ar[8].replace(",",".")+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[8].replace(",","."))/1.952).toFixed(2)+" EUR/kg"+
+                sendmsg(chid,"```cijena aluminija je :"+ar[8].replace(",",".")+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[8].replace(",","."))/1.952).toFixed(2)+" EUR/kg"+
                 "\n\t\t\t\t\t\t\t"+(parseFloat(ar[8].replace(",","."))*3.883).toFixed(2)+" HRK/kg"+
-                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[8].replace(",","."))*59.76).toFixed(2)+" RSD/kg",msid)
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[8].replace(",","."))*59.76).toFixed(2)+" RSD/kg```",msid)
                 break
             case "olovo":
-                sendmsg(chid,"cijena olova je :"+ar[11].replace(",",".")+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[11].replace(",","."))/1.952).toFixed(2)+" EUR/kg"+
+                sendmsg(chid,"```cijena olova je :"+ar[11].replace(",",".")+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[11].replace(",","."))/1.952).toFixed(2)+" EUR/kg"+
                 "\n\t\t\t\t\t\t\t"+(parseFloat(ar[11].replace(",","."))*3.883).toFixed(2)+" HRK/kg"+
-                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[11].replace(",","."))*59.76).toFixed(2)+" RSD/kg",msid)
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[11].replace(",","."))*59.76).toFixed(2)+" RSD/kg```",msid)
                 break
             case "akumulatori":
-                sendmsg(chid,"cijena akumulatora je :"+ar[12].replace(",",".")+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[12].replace(",","."))/1.952).toFixed(2)+" EUR/kg"+
+                sendmsg(chid,"```cijena akumulatora je :"+ar[12].replace(",",".")+"KM/kg\n\t\t\t\t\t\t\t"+(parseFloat(ar[12].replace(",","."))/1.952).toFixed(2)+" EUR/kg"+
                 "\n\t\t\t\t\t\t\t"+(parseFloat(ar[12].replace(",","."))*3.883).toFixed(2)+" HRK/kg"+
-                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[12].replace(",","."))*59.76).toFixed(2)+" RSD/kg",msid)
+                "\n\t\t\t\t\t\t\t"+(parseFloat(ar[12].replace(",","."))*59.76).toFixed(2)+" RSD/kg```",msid)
                 break
-	    case "prosvijetli me rombotu":
-		sendmsg(chid,await randomdova(),msid)
-		break
-            case "podsjetnik sekiju da se ubije":
-
-                if(podsjetnik!=true){
-                podsjetnik=true
-                ubijse(chid,msid)
-                }
-                break;
             default:
-                break;
+              if (vrsta.includes("podsjetnik")) {
+                if(vrsta.match("(?<=<@)(.*)[^>]")[0]&&!mrtvi.includes(vrsta.split(" ")[1])){
+                  ubijse(chid,msid,vrsta.split(" ")[1])
+                }
+              }
+              break;
         }
     }
-    async function ubijse(x,msid){
-    while (mybban==false){
-      sendmsg(x,"ubij se <@958708441554247741>",msid)
+    async function ubijse(x,msid,ctx){
+    while (true){
+      mrtvi.push(ctx)
+      sendmsg(x,"ubij se "+ctx,msid)
       await sleep(1800000);
-    }
-    mybban=false
-    }
-}
-async function getmsg(guid,msid) {
-  await sleep(300)
-  try {
-    await fetch(`https://discord.com/api/v9/channels/${guid}/messages?limit=1&around=${msid}`, {
-      "headers": {
-        "authorization": token},
-      "method": "GET",
-    }).then(r=>{return r.json()}).then(
-      x=>{
-        if (x[0]["content"].toLowerCase()=="!rombot bakar"||"!rombot zeljezo"||"!rombot aluminij"||"!rombot olovo"||"!rombot akumulatori")
-          provjeracijene(guid,x[0]["content"].toLowerCase(),msid)
-        if(x[0]["content"].toLowerCase()=="!rombot pomozi mi ciganinu")
-          sendmsg(guid,"ROMBOT STIZE U POMOC\nlista metala u magacinu je: \nbakar\nzeljezo\naluminij\nolovo\nakumulatori",msid)
-        });
-  } catch (error){
-  }
-
+    }}
 }
 function sendmsg(chid,ctx,msid) {
-    ctx=ctx.toString()
     if(ctx.match("ubij")){
         fetch(`https://discord.com/api/v9/channels/${chid}/messages`, {
     "headers": {
@@ -92,13 +61,8 @@ function sendmsg(chid,ctx,msid) {
       "content-type": "application/json"},
     "body": JSON.stringify({content:ctx}),
     "method": "POST",
-  }).then(x=>{return x.status}).then(x=>{
-	  if(x!=200){
-	    podsjetnik=false
-	    mybban=true
-	    return}
-  	  myban=false})
-      return;
+  })
+    return
     }
     fetch(`https://discord.com/api/v9/channels/${chid}/messages`, {
     "headers": {
@@ -109,6 +73,7 @@ function sendmsg(chid,ctx,msid) {
   })
 }
 async function recursive() {
+
 let socket = new WebSocket("wss://gateway.discord.gg/?v=6&encording=json");
 socket.onready = function(event){
     console.log("bot je spreman")
@@ -121,16 +86,14 @@ socket.onmessage = async function(event) {
     let e = JSON.stringify(event.data);
 
     let payload={
-        "op": 2,
-        "d": {
-            "token": token,
-            "intents": 32767,
-            "properties": {
-                "$os": "windows",
-                "$browser": "chrome",
-                "$device": 'pc'}
-            }
-        }
+      "op":2,
+      "d": {
+          "token": token,
+          "properties": {
+              "os": "NIGGERSSSS I HATE NIGGERS I HATE THEM SO MUCH",
+          },
+      }
+  };
     async function hb(socket, interval){
         while(true){
             let hbpayload={
@@ -152,9 +115,10 @@ socket.onmessage = async function(event) {
           "type": 0,}],
         "since":1,"afk":false}}))
     if (ejson["t"]=="MESSAGE_CREATE") {
-      getmsg(ejson['d']['channel_id'],ejson['d']['id'])
+      if (!ejson["d"]["content"].includes("!rombot"))
+        return
+      provjeracijene(ejson['d']['channel_id'],ejson["d"]["content"].replace("!rombot ",""),ejson['d']['id'])
     }
 }
 }
 recursive()
-
